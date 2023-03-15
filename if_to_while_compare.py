8b7be7fd5510d7e4d13bc15b14e76570bd4a53c0
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2023-03-15 10:48:40 -0500

    Added if and while examples from chapter 2.

---
+++ if_to_while_compare.py
@@ -0,0 +1,15 @@
+def if_spam():
+    spam = 0
+    if spam < 5:
+        print('Hello, world.')
+        spam = spam + 1
+
+def while_spam():
+    spam = 0
+    while spam < 5:
+        print('Hello, world.')
+        spam = spam + 1
+
+if __name__ == "__main__":
+    if_spam()
+    # while_spam()
