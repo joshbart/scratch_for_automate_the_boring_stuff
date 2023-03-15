8d62243c4cf5389ff7fddd683a047bb6f5aa6669
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2023-03-15 09:07:37 -0500

    Based off the flow chart at the beginning of chapter 2.

---
+++ is_raining.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# is_raining = api.get_weather()
+# have_umbrella = parameter.read()
+
+if is_raining == True:
+    if have_umbrella == False:
+        # do:
+            # sleep 5 minutes
+            # is_raining = api.get_weather()
+        # while (is_raining == True)
+
+print("Go outside!")
