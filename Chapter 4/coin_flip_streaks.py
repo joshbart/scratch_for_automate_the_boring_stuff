2cd301a652e77c6d2caa1649289e501550cb3095
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2023-03-22 17:06:33 -0500

    The spacing looked weird.

--- Chapter 4/coin_flip_streaks.py
+++ Chapter 4/coin_flip_streaks.py
@@ -16,11 +16,8 @@ def check_for_streaks():
     return None
 
 if __name__ == '__main__':
-
     number_of_streaks = 0
-
     for experiment in range(10000):
         coin_flip_list = generate_list_of_coin_flips()
         check_for_streaks()
-        
     print('Chance of streak: %s%%' % (number_of_streaks / 100))
