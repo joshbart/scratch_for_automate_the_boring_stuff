2cedfed28b8e4d146b6606bdaa82101631a86b29
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2023-03-22 16:52:24 -0500

    The assignment requests that we repeat this exercise 10,000 times.

--- Chapter 4/coin_flip_streaks.py
+++ Chapter 4/coin_flip_streaks.py
@@ -1,5 +1,15 @@
 def generate_list_of_coin_flips():
-    placeholder = None
+    return None
 
 def check_for_streaks():
-    placeholder = None
+    return None
+
+if __name__ == '__main__':
+
+    number_of_streaks = 0
+
+    for experiment in range(10000):
+        generate_list_of_coin_flips()
+        check_for_streaks()
+
+    print('Chance of streak: %s%%' % (number_of_streaks / 100))
