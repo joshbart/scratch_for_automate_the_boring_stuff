cf7ce8581ce2d3087885e75a2dad53d3ac7d0fed
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2023-03-22 16:13:02 -0500

    Added logic to detect the final item in the list and add 'and'.

--- Chapter 4/comma_code.py
+++ Chapter 4/comma_code.py
@@ -1,14 +1,14 @@
 def convert_list_to_string(list):
     string_from_list = ''
-    for item in list:
-        string_from_list += '%s, ' % item
+    for index, item in enumerate(list):
+        if index == (len(list) - 1):
+            string_from_list += 'and %s.' % item
+        else:
+            string_from_list += '%s, ' % item
     return string_from_list
 
-spam = [
-    'apples', 
-    'bananas', 
-    'tofu', 
-    'cats'
-]
+# spam = ['apples', 'bananas', 'tofu', 'cats']
+# spam = []
+spam = [1, 2, 3, 4, 5]
 
 print(convert_list_to_string(spam))
