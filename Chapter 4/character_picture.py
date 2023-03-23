f7c2a3f0b0b890551e1798bb45efe8290dab70cc
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2023-03-22 21:57:51 -0500

    Implemented loops.

--- Chapter 4/character_picture.py
+++ Chapter 4/character_picture.py
@@ -13,7 +13,12 @@ grid = [
 ]
 
 def print_picture(pattern):
-    return None
+    y = 0
+    while y < len(pattern[0]):
+        for x in pattern:
+            print(x[y], end='')
+        print()
+        y += 1
 
 if __name__ == '__main__':
     print_picture(grid)
