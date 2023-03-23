f9a9fd3801c076a1f5baee69b332e3b6f56262c4
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2023-03-22 20:57:51 -0500

    Implemented the logic for checking for streaks.

--- Chapter 4/coin_flip_streaks.py
+++ Chapter 4/coin_flip_streaks.py
@@ -12,12 +12,20 @@ def generate_list_of_coin_flips():
             list.append('T')
     return list
 
-def check_for_streaks():
-    return None
+def check_for_streaks(list):
+    streaks = 0
+    for index, flip_value in enumerate(list):
+        if flip_value == 'H':
+            compare_list = ['H'] * 6
+        else:
+            compare_list = ['T'] * 6
+        if compare_list == list[index:index + 6]:
+            streaks += 1
+    return streaks
 
 if __name__ == '__main__':
     number_of_streaks = 0
     for experiment in range(10000):
         coin_flip_list = generate_list_of_coin_flips()
-        check_for_streaks()
-    print('Chance of streak: %s%%' % (number_of_streaks / 100))
+        number_of_streaks += check_for_streaks(coin_flip_list)
+    print('Chance of streak: %s%%' % (number_of_streaks / (10000 * 100)))
