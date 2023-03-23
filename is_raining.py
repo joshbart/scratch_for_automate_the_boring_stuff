551353a0c56f0c2790583eb6ae472858c6cc2992
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2023-03-22 22:50:00 -0500

    Used what I've learned to start to improve this program.

--- is_raining.py
+++ is_raining.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python
 
-# is_raining = api.get_weather()
-# have_umbrella = parameter.read()
+import time
+# import requests
+# import json
 
-if is_raining == True:
-    if have_umbrella == False:
-        while is_raining == True:
-            # sleep 5 minutes
-            # is_raining = api.get_weather()
+# weather_api = requests.get(https://api.weather.gov/stations/KFAR/observations/latest)
+
+def get_weather():
+    # Need to implement code that looks up the properties.precipitationLastHour.value property from the weather_api
+    return True
+
+def read_parameters():
+    # Need to implement code to read a parameter from the command line.
+    return True
+
+if __name__ == '__main__':
+    is_raining = get_weather()
+    have_umbrella = read_parameters()
+    if is_raining == True:
+        if have_umbrella == False:
+            while is_raining == True:
+                time.sleep(300)
+                is_raining = get_weather()
 
 print("Go outside!")
